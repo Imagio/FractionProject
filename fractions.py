6c68b0b83235bdab47df84f9748e21603f7d6d0c
Vasiliy Samoylov <vasiliy.samoylov@x-tensive.com>
2020-02-12 18:28:04 +0700

    add plus operator

--- fractions.py
+++ fractions.py
@@ -1,7 +1,37 @@
+def gcd(a,b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
 class Fraction(object):
     def __init__(self, a, b):
         self.top = a
         self.bottom = b
+        t = gcd(self.top, self.bottom)
+        self.top //= t
+        self.bottom //= t
+
+    def __add__(self, other):
+        x = self
+        y = other
+        if type(y) == int:
+            y = fraction(y)
+        t = lcm(x.bottom, y.bottom)
+        kx = t // x.bottom
+        ky = t // y.bottom
+        return Fraction(kx * x.top + ky * y.top, t)
 
 
-def fraction(a, b=1): return Fraction(a, b)
+
+
+def fraction(a, b=1):
+    if type(a) != int:
+        raise Exception("Invalid argument")
+    return Fraction(a, b)
+
+
+
+
--- fractions_tests.py
+++ fractions_tests.py
@@ -8,11 +8,35 @@ class InitTestCase(unittest.TestCase):
         self.assertEqual(x.top, 1)
         self.assertEqual(x.bottom, 2)
 
+    def test_gcd(self):
+        x = fraction(39, 26)
+        self.assertEqual(x.top, 3)
+        self.assertEqual(x.bottom, 2)
+
     def test_a(self):
         x = fraction(4)
         self.assertEqual(x.top, 4)
         self.assertEqual(x.bottom, 1)
 
+    def test_invalid_a(self):
+        self.assertRaises(Exception, lambda: fraction(1.0))
+
+class AddTestCase(unittest.TestCase):
+    def test_add(self):
+        a = fraction(1, 2)
+        b = fraction(1, 3)
+        c = a + b
+        self.assertEqual(c.top, 5)
+        self.assertEqual(c.bottom, 6)
+
+    def test_add_int(self):
+        a = fraction(1, 2)
+        c = a + 3
+        self.assertEqual(c.top, 7)
+        self.assertEqual(c.bottom, 2)
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
